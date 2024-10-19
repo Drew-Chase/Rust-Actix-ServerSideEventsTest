00ffaffc8cc9d859e5f1286b3a252f985da748a9
Drew (Linux) <dcmanproductions@gmail.com>
2024-10-19 14:08:50 -0400

    Add SSE timestamp endpoint
    Integrate a new `/time` endpoint that uses Server-Sent Events (SSE) to emit timestamps in RFC3339 format every second. Updated main function to configure and run the Actix web server with this new endpoint.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
-fn main() {
-	println!("Hello, world!");
+//! Demonstrates use of the Server-Sent Events (SSE) responder.
+
+use std::{io, time::Duration};
+
+use actix_web::{get, App, HttpServer, Responder};
+use actix_web_lab::sse;
+use time::format_description::well_known::Rfc3339;
+use tokio::time::sleep;
+
+
+#[get("/time")]
+async fn timestamp() -> impl Responder {
+	let (sender, receiver) = tokio::sync::mpsc::channel(2);
+
+	actix_web::rt::spawn(async move {
+		loop {
+			let time = time::OffsetDateTime::now_utc();
+			let msg = sse::Data::new(time.format(&Rfc3339).unwrap()).event("timestamp");
+
+			if sender.send(msg.into()).await.is_err() {
+				break;
+			}
+
+			sleep(Duration::from_secs(1)).await;
+		}
+	});
+
+	sse::Sse::from_infallible_receiver(receiver).with_keep_alive(Duration::from_secs(3))
+}
+
+#[actix_web::main]
+async fn main() -> io::Result<()> {
+	HttpServer::new(|| {
+		App::new()
+			.service(timestamp)
+	})
+		.workers(2)
+		.bind(("127.0.0.1", 8080))?
+		.run()
+		.await
 }
